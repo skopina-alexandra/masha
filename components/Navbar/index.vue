4d954aa598e153ff0fb9eb50e6ab674e2b188269
Alexandra Skopina <skopina.aa@outlook.com>
2023-06-22 18:53:23 +1000

    wip - navbar

---
+++ components/Navbar/index.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+    <div class="grid grid-cols-3 flex flex-row">
+        <div class="basis-1/4">
+            <NavbarLink text="Записаться" href="#index"  />
+        </div>
+        <div class="basis-1/4">
+                <NavbarLogo class="basis-2/4"/>
+            </div>
+        <div class="grid grid-cols-1 basis-1/4">
+            <NavbarLink text="Принципы" href="#index" />
+            <NavbarLink text="Как я работаю" href="#index" />
+            <NavbarLink text="Обо мне" href="#index" />
+            
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/PawImage.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+      <img src="assets/icons/paw.svg"/>
+</template>
+
+<style scoped>
+img {
+  transform: rotate(-30deg);
+}
+</style>
