77ec235270adf9b90c7047a7367f206902848f2d
Alexandra Skopina <44308291+skopina-alexandra@users.noreply.github.com>
2023-07-16 10:17:32 +0000

    lintfix

--- components/Principles/Header.vue
+++ components/Principles/Header.vue
@@ -1,15 +1,15 @@
 <script setup>
 defineProps({
-    text: String,
-    image: String,
+  text: String,
+  image: String,
 });
 </script>
 
 <template>
-    <div class="grid grid-flow-col grid-cols-3">
-        <img :src="image">
-        <h3>
-            {{ text }}
-        </h3>
-    </div>
+  <div class="grid grid-flow-col grid-cols-3">
+    <img :src="image" />
+    <h3>
+      {{ text }}
+    </h3>
+  </div>
 </template>
--- components/Principles/Item.vue
+++ components/Principles/Item.vue
@@ -1,16 +1,15 @@
 <script setup>
 defineProps({
-    item : {
-        header: String,
-        text: String,
-        image: String,
-    }
+  item: {
+    header: String,
+    text: String,
+    image: String,
+  },
 });
 </script>
 <template>
-    <div>
-
-      <PrinciplesHeader :text="item.header" :image="item.image" />
-        <p>{{ item.text }}</p>
-    </div>
+  <div>
+    <PrinciplesHeader :text="item.header" :image="item.image" />
+    <p>{{ item.text }}</p>
+  </div>
 </template>
