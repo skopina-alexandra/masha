beb90c4e25629299298a71caa057b7bc063af020
Alexandra Skopina <skopina.aa@outlook.com>
2023-07-16 19:46:35 +1000

    add nav tag

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 
 <template>
-    <div class="grid grid-flow-row grid-rows-2">
+    <nav class="grid grid-flow-row grid-rows-2">
         <div class="grid grid-flow-col grid-cols-none justify-center">
             <a href="#top"><Logo class="self-center "/></a>
         </div>
         <div class="grid grid-flow-col cols-2 justify-between">
-            <NavbarMenuLeft class=""/>
-            <NavbarMenuRight  class=""/>
+            <NavbarMenuLeft />
+            <NavbarMenuRight  />
         </div>
     </nav>
 
