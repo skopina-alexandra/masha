1bae1285a631caeb301698c4819e02175a1b1418
Alexandra Skopina <44308291+skopina-alexandra@users.noreply.github.com>
2023-07-21 03:02:02 +0000

    remove index page comments

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <!-- <div class = "h-screen flex flex-col"> -->
-  <!-- <Navbar class="pt-5 px-5" /> -->
-  <!-- <Welcome class="px-10 my-auto" /> -->
-  <!-- </div> -->
+  <div class = "h-screen flex flex-col">
+  <Navbar class="pt-5 px-5" />
+  <Welcome class="px-10 my-auto" />
+  </div>
   <div class="h-screen flex flex-col">
     <Principles class="w-2/3 mx-auto" />
   </div>
